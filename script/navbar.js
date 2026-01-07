7a231b1c9f88a34c887242b2ed765f07701bcafd
Wejkey <184445277+wejkey@users.noreply.github.com>
2026-01-07 20:45:10 +0100

    Release changed to April 3

--- script/navbar.js
+++ script/navbar.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
       <a href="${discordLink}">Discord</a>
     </nav>
     <div class="top-actions">
-      <button class="cta" type="button">Spustenie: Jún 1, 2026</button>
+      <button class="cta" type="button">Spustenie: Apríl 3, 2026</button>
     </div>
   `;
 
