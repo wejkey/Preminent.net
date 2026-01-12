6da1a2efe0474fce10e0132a6048ccf5f4366acd
Wejkey <184445277+wejkey@users.noreply.github.com>
2026-01-12 23:02:32 +0100

    Add files via upload
    Changed release date

--- script/navbar.js
+++ script/navbar.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
       <a href="${discordLink}">Discord</a>
     </nav>
     <div class="top-actions">
-      <button class="cta" type="button">Spustenie: Apríl 3, 2026</button>
+      <button class="cta" type="button">Spustenie: Február 27, 2026</button>
     </div>
   `;
 
